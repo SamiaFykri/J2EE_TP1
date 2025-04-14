f4aff56456c28c8d8ab178cb3bd746b08336848f
SamiaFykri <samiafykri06@gmail.com>
2025-04-14 16:21:42 +0200

    Ajout de l'activité 1 pour inversion de controle et gestion de dépendances

---
+++ TP1_J2E/tp1/src/main/java/metier/MetierImpl.java
@@ -0,0 +1,18 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao; // Couplage faible
+
+    // Injection des dépendances via le constructeur
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        double t = dao.getData();
+        return t * 23 / 3;
+    }
+}
